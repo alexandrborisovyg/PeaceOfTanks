e63bb24809eeb1a4d5d44586d272a2d8c59d865a
Alexandr Borisov <messivo_@outlook.com>
2022-11-14 02:51:20 +0300

    Remove bin, obj, fix font release issue, migrate to .NET Framework 4.8, update gitignore

--- Peace_of_Tanks/Properties/AssemblyInfo.cs
+++ Peace_of_Tanks/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,7 +7,7 @@ using System.Runtime.InteropServices;
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
 [assembly: AssemblyTitle("Peace_of_Tanks")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Windows version of old game Battle City")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Peace_of_Tanks")]
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: NeutralResourcesLanguage("ru")]
